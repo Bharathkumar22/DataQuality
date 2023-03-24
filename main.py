ba58ffd57954b70f1325a30f63bcce6a3525049b
bharathmittapally <mbharathkumar9@gmail.com>
2023-03-23 20:51:52 -0400

    DQ Initial Framework

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import pandas as pd
+from pyspark.sql import SparkSession
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Initialize Spark
+spark = SparkSession.builder.appName('DataQualityCheck').getOrCreate()
 
+# Read data from Oracle and S3
+oracle_df = spark.read.format("jdbc").option("url", "<Oracle URL>").option("dbtable", "<Oracle table>").load()
+s3_df = spark.read.format("csv").option("header", "true").load("<S3 file path>")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Convert data to Pandas dataframe
+oracle_pandas_df = oracle_df.toPandas()
+s3_pandas_df = s3_df.toPandas()
 
+# Define data quality rules
+def compare_dataframes(df1, df2):
+    assert df1.equals(df2), 'Dataframes are not equal'
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Compare dataframes and generate report with highlighted differences
+try:
+    compare_dataframes(oracle_pandas_df, s3_pandas_df)
+    print('Data quality check passed')
+except AssertionError as error:
+    print('Data quality check failed')
+    # highlight differences in the two dataframes
+    df_diff = oracle_pandas_df.compare(s3_pandas_df)
+    df_styled = df_diff.style.applymap(lambda x: 'background-color: yellow', subset=pd.IndexSlice[:, df_diff.columns])
+    display(df_styled)
